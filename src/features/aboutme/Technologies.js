da06f890fa87250d92c81b63958ec227a3aaba95
Hein Htet <heinha123456@gmail.com>
2023-01-08 19:43:05 +0630

    technologies bullet change

--- src/features/aboutme/Technologies.js
+++ src/features/aboutme/Technologies.js
@@ -7,31 +7,31 @@ const Technologies = () => {
         Here are a few technologies I've been working with recently.
       </h4>
       <ul className="grid grid-cols-2 md:grid-cols-3 text-gray gap-x-6">
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           JavaScript (ES6+)
         </li>
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           React
         </li>
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           Node.js
         </li>
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           Express.js
         </li>
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           MongoDB
         </li>
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           PostgreSQL
         </li>
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           Tailwind <span className="text-sm">(favourite)</span>
         </li>
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           MaterialUI
         </li>
-        <li className="relative mb-2 pl-4 before:content-['∽'] before:absolute before:left-0">
+        <li className="relative mb-2 pl-4 before:content-['-'] before:absolute before:left-0">
           Bootstrap
         </li>
       </ul>
