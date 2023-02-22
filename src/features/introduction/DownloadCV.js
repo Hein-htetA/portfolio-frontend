9f0fe443cd4208ef967e283c1b784087c26614ae
Hein Htet <heinha123456@gmail.com>
2023-02-22 18:20:36 +0630

    pulled readme

--- src/features/introduction/DownloadCV.js
+++ src/features/introduction/DownloadCV.js
@@ -1,5 +1,5 @@
 import React from "react";
-import cv from "../../assets/Certificates.pdf";
+import cv from "../../assets/HeinHACV.pdf";
 import certificate from "../../assets/Certificates.pdf";
 
 const DownloadCV = () => {
